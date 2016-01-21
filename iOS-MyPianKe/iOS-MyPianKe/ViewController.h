75ab4edcfe6de32da4cda9de6738ccaec410f399
WhoMessi <690159778@qq.com>
2016-01-21 15:54:37 +0800

    刚创建的

---
+++ iOS-MyPianKe/iOS-MyPianKe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-MyPianKe
+//
+//  Created by ma c on 16/1/21.
+//  Copyright © 2016年 戴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS-MyPianKe/iOS-MyPianKe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS-MyPianKe
+//
+//  Created by ma c on 16/1/21.
+//  Copyright © 2016年 戴. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@".........");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
